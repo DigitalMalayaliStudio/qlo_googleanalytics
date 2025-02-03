72bdb881a8ced0e9c602943fbc736fee08078307
zcraber <shibinjose991@gmail.com>
2025-02-03 17:34:05 +0530

    🐛fix: error "No template found for module qlo_googleanalytics"

--- qlo_googleanalytics.php
+++ qlo_googleanalytics.php
@@ -51,7 +51,7 @@ class Qlo_Googleanalytics extends Module
     {
         $this->name = 'qlo_googleanalytics';
         $this->tab = 'analytics_stats';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
         $this->author = 'PrestaShop';
         $this->bootstrap = true;
